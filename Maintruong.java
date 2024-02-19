2d6b157bf94ee8e801146cbccb9f5c03f60e3b89
Tuan241025 <142655165+Tuan241025@users.noreply.github.com>
2024-02-19 16:03:34 +0700

    Add files via upload

---
+++ Maintruong.java
@@ -0,0 +1,19 @@
+package truonghocvanlang;
+
+public class Maintruong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Sinhvien s = new Sinhvien();
+//		SVCD scd = new SVCD();
+//		s.Nhap();
+//		s.Xuat();
+//		scd.Nhap();
+//		scd.Xuat();
+		SVDH s = new SVDH();
+		s.Nhap();
+		s.Xuat();
+		
+	}
+
+}
