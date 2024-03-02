31bcf1104b8675ba5d12e9e48e3a48a162d81e58
Tuan241025 <142655165+Tuan241025@users.noreply.github.com>
2024-03-02 15:18:03 +0700

    quanlydienthoai

--- Main.java
+++ Main.java
@@ -1,22 +1,41 @@
-package info;
+import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		NhanVien nv = new NhanVien();
-//		NVSX nv1 = new NVSX();
-//		nv.Nhap();
-//		nv.Xuat();
-//		nv1.Nhap();
-//		nv1.Xuat();
+	Scanner sc = new Scanner(System.in);
+//	public void Giacaonhat() {
+//		Double giacaonhat = 0.0;
+//		Double GiaSP = Double.parseDouble(sc.nextLine());
 //		
 //	}
-//		NVCN nv = new NVCN();
-//		nv.Nhap();
-//		nv.Xuat();
-		NVQL nv = new NVQL();
-		nv.Nhap();
-		nv.Xuat();
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DienThoai[] DienthoaiArray = new DienThoai[5];
+		for (int i = 0; i < DienthoaiArray.length; i++) {
+			System.out.println("\n\t-----Thong Tin Cua Dien Thoai-----");
+			DienthoaiArray[i] = new DienThoai();
+			DienthoaiArray[i].Nhap();
+			DienthoaiArray[i].Xuat();
+		}
+		double giacaonhat;
+		int vt = 0;
+		giacaonhat = DienthoaiArray[0].getGiaSP();
+		for (int i = 0; i < DienthoaiArray.length; i++) {
+			if( giacaonhat < DienthoaiArray[i].getGiaSP()) {
+				giacaonhat = DienthoaiArray[i].getGiaSP();
+				vt = i;
+		}
+			
+		}
+		System.out.println("San Pham Co Gia Cao Nhat La ");
+		DienthoaiArray[vt].Xuat();
+		System.out.println("\n\t---Dien Thoai Samsung---");
+		String Tensp = DienthoaiArray[vt].TenSP;
+		for (int i = 0; i < DienthoaiArray.length; i++) {
+			if( DienthoaiArray[i].getTenSP().equalsIgnoreCase("Samsung")) {
+				DienthoaiArray[i].Xuat();
+			}
+		}
+	}
+	
+
 }
